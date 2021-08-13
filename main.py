f3dcf93a2011b192dfc8d6f8ee6947e9e195a7a3
BizzyPythonBear <bizzeebear@hotmail.com>
2021-08-12 17:11:25 -0700

    Large update: All linux commands installed into /usr/bin/ will now work

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import subprocess
+import asyncio
 
 env = {}
-v = "1.1.1"
+v = "2.1.1"
 env['TERM'] = "linux"
 cwd = os.getcwd()
 c = input(f"[{cwd}]- #: ")
@@ -23,6 +24,20 @@ Bearbones Help Screen
 	exit: Closes Shell
 	Every command you used in bash, works with this shell!""")
 
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
+
 def commands():
 	while True:
 		c = input(f"[{cwd}]- #: ")
@@ -31,19 +46,20 @@ def commands():
 		commandSplit = command.split()
 		firstPart = commandSplit[0]
 		comCheck = os.path.isfile(f"{comDir}/{firstPart}")
-		p = os.fork()
 		if len(commandSplit) > 1:
 			args = commandSplit[1]
 			firstPart = commandSplit[0]
-			ran = subprocess.run([firstPart, "-l", args], capture_output=True,text=True, shell=True)
-			print(ran.stdout)
+			asyncio.run(run(f'{firstPart} {args}'))
+			print('\n')
 		elif len(commandSplit) == 1:
 			args = ""
 			firstPart = commandSplit[0]
-			if firstPart != 'help':
-				ran = subprocess.run([firstPart], capture_output=True,text=True, shell=True)
-				print(ran.stdout)
-			if firstPart == 'exit':
+			if firstPart != 'help' and 'exit':
+				subprocess.run([firstPart], capture_output=True,text=True, shell=True)
+				print('\n')
+			elif firstPart == 'exit':
 				sys.exit(-1)
 				exit(-1)
+			else:
+				print("Not a command!")
 commands()
